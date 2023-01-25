6dd2bc308317ee53f5ea93d9e32c8dd6dc8f08b4
Srenidhi <srenidhi@nstore.in>
2023-01-25 16:41:02 +0530

    comment added for click

--- app/src/main/java/com/rak_developer/haritmartkotlin/util/Click.kt
+++ app/src/main/java/com/rak_developer/haritmartkotlin/util/Click.kt
@@ -3,6 +3,7 @@ package com.rak_developer.haritmartkotlin.util
 import android.view.View
 
 object Click {
+    //double click prevent
     fun preventTwoClick(view: View) {
         view.isEnabled = false
         view.postDelayed({ view.isEnabled = true }, 1000)
